1092a02189dba3e60d2161bb35c8c5318b01642e
banon jonathan <j.banon01@gmail.com>
2021-05-26 20:46:56 +0200

    routing fixed

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -17,13 +20,15 @@ class ProgramController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
     /**
-     * @Route("/{id}/", requirements={"id"="\d+"}, methods={"GET"} name="program_show")
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
      */
     public function show(int $id): Response
     {
         return $this->render('program/show.html.twig', [
-            'id' => $id
+            'id' => $id,
         ]);
     }
+
 }
