0a9ed4abfef3782ff25418fbc8039ccc9533fb2d
banon jonathan <j.banon01@gmail.com>
2021-06-06 16:56:36 +0200

    quest-12 done

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route ("/categories", name="category_")
@@ -25,6 +27,30 @@ class CategoryController extends AbstractController
             ->findAll();
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     *The controller for the category add form
+     *
+     *@Route("/new", name="new")
+     *@return Response
+     */
+    public function new(Request $request):  Response
+    {
+        $category = new Category();
+        // Create associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Create data from HTTP request
+        $form->handleRequest($request);
+        if($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+           "form" => $form->createView(),
         ]);
     }
 
